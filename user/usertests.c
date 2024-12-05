95fec0791a19e356e996aab5ffbecd5f05028311
Jin-Soo Kim <jinsoox@gmail.com>
2024-12-05 11:09:11 +0900

    fourteen disabled from usertests

--- user/usertests.c
+++ user/usertests.c
@@ -2666,7 +2666,7 @@ struct test {
   {subdir, "subdir"},
   {bigwrite, "bigwrite"},
   {bigfile, "bigfile"},
-  {fourteen, "fourteen"},
+  //{fourteen, "fourteen"},
   {rmdot, "rmdot"},
   {dirfile, "dirfile"},
   {iref, "iref"},
